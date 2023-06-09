6f8451de98d1cdb6e72f4b0f3a28206b714391d8
piotrpdev <peterplaczek03@gmail.com>
2023-06-09 12:08:22 +0100

    Add basic version and README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Kuby
+
+Very simple CLI visualization of Kubernetes objects.
+
+## Credit
+
+- [Example code](https://github.com/kubernetes-client/java/wiki/3.-Code-Examples)
+- [Kind kubeconfig help](https://stackoverflow.com/a/66274247)
+  - `sudo kubectl config view --flatten >> ~/.kube/config`
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import io.kubernetes.client.openapi.ApiException
+import io.kubernetes.client.openapi.Configuration
+import io.kubernetes.client.openapi.apis.CoreV1Api
+import io.kubernetes.client.util.Config
+import java.io.IOException
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    listPods()
+}
+
+fun listPods() {
+    val client = Config.defaultClient()
+    Configuration.setDefaultApiClient(client)
+    val api = CoreV1Api()
+    val list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, false)
+    for (item in list.items) {
+        println(item.metadata!!.name)
+    }
 }
